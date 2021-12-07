226729f187e11678ce7e6abf999da88c16d78444
venkateshbaddela <84497133+venkateshbaddela@users.noreply.github.com>
2021-12-07 11:11:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,11 @@
 
 const body = document.querySelector("body");
 
-
-
 const toggleBtn = document.querySelector(".header-toggle__con-btn");
-
-
 
 toggleBtn.addEventListener("click", function (e) {
   if (e.target.checked) body.classList.add("dark");
   else body.classList.remove("dark");
 });
 
-let lightTheme = {};
+
